18b78a66f709582ca65e2846a7afbedaf718da45
Yaroslav <yaroslav7v@gmail.com>
2025-07-23 00:05:02 +0300

    new changes

--- models/general.model.js
+++ models/general.model.js
@@ -2,9 +2,12 @@ const mongoose = require('mongoose');
 
 const GeneralSchema = new mongoose.Schema({
     sitePaused: { type: Boolean, default: false },
-    normalSlots: { type: Number, required: false },
-    expressSlots: { type: Number, required: false },
-    fastSlots: { type: Number, required: false },
+    kzNormalSlots: { type: Number, default: 0 },
+    kzExpressSlots: { type: Number, default: 0 },
+    kzFastSlots: { type: Number, default: 0 },
+    uaNormalSlots: { type: Number, default: 0 },
+    uaExpressSlots: { type: Number, default: 0 },
+    uaFastSlots: { type: Number, default: 0 },
 });
 
 module.exports = mongoose.model('General', GeneralSchema);
