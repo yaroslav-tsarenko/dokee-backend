2b950efc7514b6e9a6aa96beccf86617549dd1fb
Yaroslav <yaroslav7v@gmail.com>
2025-07-24 22:15:59 +0300

    new changes

--- controllers/general.controller.js
+++ controllers/general.controller.js
@@ -1,4 +1,5 @@
-const General = require('../models/general.model');
+import General from '../models/general.model.js';
+import crypto from 'crypto';
 
 const getGeneral = async (req, res) => {
     let general = await General.findOne();
@@ -22,4 +23,56 @@ const updateGeneral = async (req, res) => {
     res.json(general);
 };
 
-module.exports = { getGeneral, updateGeneral };
+const initWayforpayPayment = async (req, res) => {
+    const {
+        email,
+        totalValue,
+        productName = 'Document translation',
+        orderReference = 'ORD-' + Date.now()
+    } = req.body;
+
+    const merchantAccount = 'www_dokee_pro';
+    const merchantSecretKey = '43f8ec5981329304f616266597336518739b69c';
+    const merchantDomainName = 'www.dokee.pro';
+    const currency = 'KZT';
+    const amount = parseFloat(totalValue).toFixed(2);
+    const orderDate = Math.floor(Date.now() / 1000);
+
+    const productNames = [productName];
+    const productCounts = ['1'];
+    const productPrices = [amount];
+
+    const signatureSource = [
+        merchantAccount,
+        merchantDomainName,
+        orderReference,
+        orderDate.toString(),
+        amount,
+        currency,
+        ...productNames,
+        ...productCounts,
+        ...productPrices
+    ].join(';');
+
+    const merchantSignature = crypto
+        .createHash('md5')
+        .update(signatureSource + merchantSecretKey)
+        .digest('hex');
+
+    return res.status(200).json({
+        merchantAccount,
+        merchantDomainName,
+        orderReference,
+        orderDate,
+        amount,
+        currency,
+        productName: productNames,
+        productCount: productCounts,
+        productPrice: productPrices,
+        clientEmail: email,
+        merchantSignature
+    });
+};
+
+
+export { getGeneral, updateGeneral, initWayforpayPayment };
--- routes/general.route.js
+++ routes/general.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import {getGeneral, updateGeneral} from '../controllers/general.controller.js';
+import {getGeneral, initWayforpayPayment, updateGeneral} from '../controllers/general.controller.js';
 
 const router = express.Router();
 
 router.get('/get-general-settings', getGeneral );
 router.put('/update-general', updateGeneral);
+router.post('/payment-init', initWayforpayPayment);
 
 export default router;
